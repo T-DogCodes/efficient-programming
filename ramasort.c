d96ee1cfcb0b746e6550366560255a8b09f5c82c
Thomas Stoiber <e11777755@student.tuwien.ac.at>
2023-01-02 11:19:35 +0100

    Finished Optimization

--- ramasort.c
+++ ramasort.c
@@ -13,6 +13,8 @@
 #include <stdlib.h>
 #include <string.h>
 
+#define MEMORY_LIMIT 50000
+
 struct entry {
     long value;
 };
@@ -41,6 +43,8 @@ int main(int argc, char **argv) {
     char *endptr;
 
     long i, j;
+    long i_3  = 0;
+    long result;
     long m = 0;
 
     // cubes array
@@ -51,6 +55,7 @@ int main(int argc, char **argv) {
     // result variables
     long count = 0;
     long checksum = 0;
+    long slice_count = 0;
 
     // USAGE & Argument Parsing
     if (argc != 2) {
@@ -65,25 +70,37 @@ int main(int argc, char **argv) {
 
     long slice_min, slice_max;
     for (slice_min = 0; slice_min < maximum_number; slice_min = slice_max) {
-        slice_max = slice_min + 100000;
+        slice_max = slice_min;
+        slice_count++;
+
+        do {
+            slice_max += 10000;
+            slice_table_size = size_table(slice_max) - size_table(slice_min) + 100;
+        } while(slice_table_size < MEMORY_LIMIT);
 
         m = 0;
-        slice_table_size = size_table(slice_max) - size_table(slice_min) + 100;
         slice_table = calloc(slice_table_size, sizeof(struct entry));
 
         if (slice_table_size > table_size) {
             table_size = slice_table_size;
         }
 
-        for (i = 0; cube(i) <= slice_max; i++) {
-            for (j = i + 1; cube(i) + cube(j) <= slice_max; j++) {
-                if (cube(i) + cube(j) > slice_min) {
-                    slice_table[m++] = (struct entry){cube(i) + cube(j)};
+        i = 0;
+        i_3 = cube(i);
+        while (i_3 + i_3 <= slice_max) {
+            j = (long) (cbrt((double) (slice_min - i_3)));
+            if (j <= i) {
+                j = i + 1;
+            }
+            for (; (result = i_3 + cube(j)) <= slice_max; j++) {
+                if (result > slice_min) {
+                    slice_table[m++] = (struct entry){result};
                 }
             }
+            i++;
+            i_3 = cube(i);
         }
 
-        assert(m <= slice_table_size);
         qsort(slice_table, m, sizeof(struct entry), comp_entry);
         for (i = 1; i < m; i++) {
             if (slice_table[i - 1].value == slice_table[i].value) {
@@ -99,6 +116,7 @@ int main(int argc, char **argv) {
     // Printing
     printf("%ld Ramanujan numbers up to %ld, checksum=%ld\noccupation=%ld, size=%ld\n", count, maximum_number, checksum, m, table_size);
     printf("Memory usage: >=%ld\n", table_size * sizeof(struct entry));
+    printf("Slice Cout: %ld", slice_count);
     return 0;
 
 usage:
